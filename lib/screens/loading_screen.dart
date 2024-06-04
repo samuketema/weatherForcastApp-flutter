12e9e6f6e2f016cce436fb54e4391697169a2b30
Samuel <samuelketema01@gmail.com>
2024-06-05 01:40:29 +0300

    Modification

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +9,29 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  
+@override
+  void initState() {
+    super.initState();
+    getlocation();
+  }
+
+ Future getlocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+        print(position.latitude);
+        print(position.latitude);
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            //Get the current location
+           
           },
           child: Text('Get Location'),
         ),
@@ -20,3 +39,5 @@ class _LoadingScreenState extends State<LoadingScreen> {
     );
   }
 }
+
+
